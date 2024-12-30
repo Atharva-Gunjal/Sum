69f1555dab73385b7755184a284f38f13616ffa1
Atharva-Gunjal <145995692+Atharva-Gunjal@users.noreply.github.com>
2024-12-30 13:18:47 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,12 @@
+public class Sum
+{
+    public static void main(String []args)
+    {
+      int a=5;
+      int b=10;
+      int c;
+       
+      c=a+b;
+      System.out.println("Addition is:-" +c);
+    }
+}
